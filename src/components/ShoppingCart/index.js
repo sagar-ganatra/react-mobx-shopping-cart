b2f433b94d5ccca09c882765cea832cb3bc19b44
sagar-ganatra <sagar.shg@gmail.com>
2018-01-26 18:11:40 +0530

    Adding build

--- src/components/ShoppingCart/index.js
+++ src/components/ShoppingCart/index.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import Book from '../Book/';
-import './ShoppingCart.css';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import Book from "../Book/";
+import "./ShoppingCart.css";
 
 @observer
 class ShoppingCart extends Component {
-    render() {
-        return (
-            <div className="shopping-cart-component">
-                <header>Shopping Cart (Total Count: {this.props.cart.bagCount})</header>
-                <ul className="cart-list">
-                {
-                    this.props.cart.itemsInBag.map(item => {
-                        return (
-                            <li className="cart-item"
-                                key={`shopping-cart-${item.book.id}`}>
-                                <Book book={item.book} />
-                                <div>Count: {item.count}</div>
-                            </li>
-                        );
-                    })
-                }
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="shopping-cart-component">
+        <header>Shopping Cart (Total Count: {this.props.cart.bagCount})</header>
+        <ul className="cart-list">
+          {this.props.cart.itemsInBag.map(item => {
+            return (
+              <li className="cart-item" key={`shopping-cart-${item.book.id}`}>
+                <Book book={item.book} />
+                <div>Count: {item.count}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ShoppingCart;
